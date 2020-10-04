308859a5be5efdc90f42753b208e1b01ce940b8c
Megan Phillips <mnphillips2020@gmail.com>
2020-10-04 18:55:26 -0400

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Megan Phillips on 10/4/20.
+//  Copyright © 2020 Megan Phillips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
